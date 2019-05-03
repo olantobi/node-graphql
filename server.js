a863966a88a5b513105de852efed95faf5a7b6e1
Ebenezer Olanrewaju <ebenezer.olanrewaju@venturegardengroup.com>
2019-05-03 07:33:09 +0100

    Setup graphql editor page

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
 import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
  
-const server = express();
+const app = express();
 
-server.listen(4000, () => {
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+
+    schema {
+        query: Query
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello(root) {
+            return 'world';
+        }
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers,
+    playground: {
+        endpoint: 'http://localhost:4000/graphql',
+        settings: {
+            'editor.theme': 'light'
+        }
+    }
+});
+
+server.applyMiddleware({app: app});
+// var schema = makeExecutableSchema({ typeDefs, resolvers });
+// server.use('/graphql', graphqlExpress());
+// server.use('/graphiql', graphiqlExpress({endpointURL: "/graphql"}));
+
+app.listen(4000, () => {
     console.log('Express app listening on port 4000.');
 });
